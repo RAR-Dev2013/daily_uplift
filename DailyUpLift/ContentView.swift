0d295e3944084ff95f224dc84d635c0112f3e087
Roxanne <rreindl13@gmail.com>
2025-06-28 10:24:02 -0500

    Initial commit of Daily Uplift app

--- DailyUpLift/ContentView.swift
+++ DailyUpLift/ContentView.swift
@@ -1,24 +1,29 @@
-//
-//  ContentView.swift
-//  DailyUpLift
-//
-//  Created by Roxanne Reindl on 6/21/25.
-//
-
 import SwiftUI
 
+
 struct ContentView: View {
+    var quotes = ["The only way to do great work is to love what you do.", "Your time is limited, don't waste it living someone else's life.", "Believe you can and you're halfway there."]
+
+
+@State private var currentQuote = "GO YOU"
+
+    
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            Text(quotes.randomElement()!)
+                .bold()
+                .font(.system(size: 40, weight: .bold))
+            Spacer()
+            Text(currentQuote)
+                .font(.title)
+            Button("Another Quote") {
+                currentQuote = quotes.randomElement()!
+            }
+            Text("Built by: Rox and CJ")
+                .font(.footnote)
+                .padding(.bottom)
         }
-        .padding()
     }
-}
-
-#Preview {
-    ContentView()
 }
