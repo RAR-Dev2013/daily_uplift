fb3efe3b95386480d5fd7fab51f87dea944e4001
Roxanne <rreindl13@gmail.com>
2025-07-01 20:50:51 -0500

    Added background color and top to bottom gradient

--- DailyUpLift/ContentView.swift
+++ DailyUpLift/ContentView.swift
@@ -15,83 +15,99 @@ struct ContentView: View {
         quotes.randomElement()!
     }
     
-
-    
-    
     var body: some View {
-
-        VStack {
+        
+        ZStack {
+            LinearGradient(
+                    gradient: Gradient(colors: [
+                    
+                        Color(red: 0.95, green: 0.9, blue: 0.9), // Light aqua top
+                        Color(red: 0.0, green: 0.6, blue: 0.7)   // Deep teal bottom
+                    ]),
+                    startPoint: .top,
+                    endPoint: .bottom
+                )
+                .ignoresSafeArea()
             
-            if isHomeScreen {
+            VStack {
                 
-                //Welcome Sign
-                
-                Text(welcomeSign)
-                    .font(.system(size: 40, weight: .bold))
-                Spacer()
-                
-                //First Daily Lift
-                Text(welcomeMessage)
-                    .font(.system(size: 30, weight: .bold))
-
-                
-                Spacer()
-                
-                //Button to generate daily lift
-                Button("DAILY LIFT") {
-                    isHomeScreen = false
-                    currentQuote = quotes.randomElement()!
+                if isHomeScreen {
                     
-                }
-                .font(.title2)
-                .padding()
-                .background(Color.pink)
-                .foregroundColor(.white)
-                .cornerRadius(50)
-                Spacer()
-                
-                //Footer Text
-                Text("Built by: RARE_Dev")
-                    .font(.footnote)
-                    .padding(.bottom)
-                
-            } else {
-                
-                //DAILY LIFT PAGE
-                
-                //New generated quote
-                Text(currentQuote)
-                    .font(.system(size: 30, weight: .semibold))
-                    .multilineTextAlignment(.center)
+                    //Welcome Sign
+                    
+                    Text(welcomeSign)
+                        .font(.system(size: 40, weight: .bold))
+                    Spacer()
+                    
+                    //First Daily Lift
+                    Text(welcomeMessage)
+                        .font(.system(size: 30, weight: .bold))
+                    
+                    
+                    Spacer()
+                    
+                    //Button to generate daily lift
+                    Button("DAILY LIFT") {
+                        isHomeScreen = false
+                        currentQuote = quotes.randomElement()!
+                        
+                    }
+                    .font(.title2)
                     .padding()
-                
-                //Button generates new quote
-                Button("Another Lift"){
-                    isHomeScreen = false
-                    currentQuote = getRandomQuote()
+                    .background(Color.blue)
+                    .foregroundColor(.black)
+                    .cornerRadius(50)
+                    Spacer()
                     
-                }
+                    //Footer Text
+                    Text("Built by: RARE_Dev")
+                        .font(.footnote)
+                        .padding(.bottom)
+                    
+                } else {
+                    
+                    //DAILY LIFT PAGE
+                    
+                    //New generated quote
+                    Text(currentQuote)
+                        .font(.system(size: 30, weight: .semibold))
+                        .multilineTextAlignment(.center)
+                        .padding()
+                    
+                    //Button generates new quote
+                    Button("Another Lift"){
+                        isHomeScreen = false
+                        currentQuote = getRandomQuote()
+                        
+                    }
                     .background(Color.pink)
                     .foregroundColor(.white)
                     .cornerRadius(50)
-                
-                //Button returns user home
-                Button("HOME"){
-                    isHomeScreen = true
                     
-                }
+                    //Button returns user home
+                    Button("HOME"){
+                        isHomeScreen = true
+                        
+                    }
                     .background(Color.pink)
                     .foregroundColor(.white)
                     .cornerRadius(50)
-                
-                
-                
-                
+                    
+                    
+                    
+                    
+                    
+                }
                 
             }
             
+            
+            
         }
+        
+        
     }
+    
 }
 
 
